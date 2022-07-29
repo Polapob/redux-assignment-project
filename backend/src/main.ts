1b51f55346fcc69cafd60dc8e5b4573d9d7344a9
Polapob <71330294+Polapob@users.noreply.github.com>
2022-07-29 13:02:32 +0700

    enable cors to localhost:3000

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,6 +5,11 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  });
   await app.listen(process.env.PORT);
   app.use(cookieParser());
   const prismaService = app.get(PrismaService);
