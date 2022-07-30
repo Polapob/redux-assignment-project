a6c8d08147eb5565df208a14175c85af7fcf73e9
Polapob <71330294+Polapob@users.noreply.github.com>
2022-07-30 20:21:47 +0700

    add handleLogin Post login to reducer

--- frontend/src/store/features/auth/reducer.ts
+++ frontend/src/store/features/auth/reducer.ts
@@ -1,6 +1,7 @@
 import { ActionReducerMapBuilder, createReducer } from "@reduxjs/toolkit";
 import { InitialAuthInterface, initialState, LoadingStatus } from "./state";
-import { handleLoginPost } from "./thunk";
+import handleLoginPost from "./thunk/handleLoginPost";
+import handleRegisterPost from "./thunk/handleRegisterPost";
 
 export const createReducers = () => {
   return {};
@@ -13,10 +14,18 @@ export const createExtraReducers = <T>() => {
         return { ...state, error: "", loading: LoadingStatus.FINISH, sessionId: action.payload.sessionId };
       })
       .addCase(handleLoginPost.rejected, (state, action) => {
-        console.log(action);
-        return { ...state, error: "error occured!", loading: LoadingStatus.ERROR };
+        return { ...state, error: action.payload?.message, loading: LoadingStatus.ERROR };
       })
-      .addCase(handleLoginPost.pending, (state, action) => {
+      .addCase(handleLoginPost.pending, (state) => {
+        return { ...state, error: "", loading: LoadingStatus.LOADING };
+      })
+      .addCase(handleRegisterPost.fulfilled, (state, action) => {
+        return { ...state, error: "", loading: LoadingStatus.FINISH };
+      })
+      .addCase(handleRegisterPost.rejected, (state, action) => {
+        return { ...state, error: action.payload?.message, loading: LoadingStatus.ERROR };
+      })
+      .addCase(handleRegisterPost.pending, (state, action) => {
         return { ...state, error: "", loading: LoadingStatus.LOADING };
       });
   };
