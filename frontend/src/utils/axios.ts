d22b89313a3a3bfafd8c4e8cb0f43de3eb26026a
Polapob <71330294+Polapob@users.noreply.github.com>
2022-07-30 08:24:36 +0700

    remove console.log

--- frontend/src/utils/axios.ts
+++ frontend/src/utils/axios.ts
@@ -1,6 +1,5 @@
 import axios from "axios";
 
-console.log("url =", process.env.NEXT_PUBLIC_API_URL);
 const apiClient = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
   withCredentials: true,
