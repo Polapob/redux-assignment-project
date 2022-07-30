b63970f3793c417f02f93d74bfc8cfe48844b352
Polapob <71330294+Polapob@users.noreply.github.com>
2022-07-30 09:51:31 +0700

    register nest's cache module

--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -1,13 +1,25 @@
-import { Module } from '@nestjs/common';
+import {
+  CacheModule,
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+} from '@nestjs/common';
+import { AuthMiddleware } from 'src/auth/auth.middleware';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { RedisCacheModule } from 'src/redis/redis.module';
+import { RedisCacheService } from 'src/redis/redis.service';
 import { UserController } from './user.controller';
 import { UserRepository } from './user.repository';
 import { UserService } from './user.service';
 
 @Module({
-  imports: [],
-  providers: [PrismaService, UserRepository, UserService],
+  imports: [RedisCacheModule, CacheModule.register({ ttl: 5, max: 10 })],
+  providers: [PrismaService, UserRepository, UserService, RedisCacheService],
   controllers: [UserController],
   exports: [UserService],
 })
-export class UserModule {}
+export class UserModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AuthMiddleware).forRoutes(UserController);
+  }
+}
